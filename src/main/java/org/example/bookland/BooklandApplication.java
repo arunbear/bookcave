e2e2454cf2db546ac1de988cc8f01afa2f44773b
Arun Prasaad <arunbear@cpan.org>
2026-02-27 13:11:40 +0000

    Add initial project files

---
+++ src/main/java/org/example/bookland/BooklandApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookland/BooklandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
